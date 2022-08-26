b48a43c70a8ba3e82c3487aef73353a5f9e8b174
ThatianneF <100847885+ThatianneF@users.noreply.github.com>
2022-08-26 00:21:22 -0300

    more

--- cypress/e2e/produtos.spec.cy.js
+++ cypress/e2e/produtos.spec.cy.js
@@ -1,12 +1,22 @@
 describe('Funcionalidade da página de produtos', () => {
-});
+
   beforeEach(() => {
-    cy.visit ('/minha-conta/')
-  
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
   });
 
-  it.only('Deve selecionar um produto da lista', () => {
-    cy.get('[class="product-block- grid"]')
+  it('Deve selecionar um produto da lista', () => {
+    cy.get('[class="product-block grid"]')
+      .eq(5)
+      .click()
 
+  });
 
-});
+  it.only('Deve selecionar um produto ao carrinho', () => {
+    cy.get('[class="product-block grid"]')
+      .eq(5).click()
+      cy.get('.button-variable-item-33').click()
+      cy.get('.button-variable-item-Red').click()
+      cy.get('.input-text').click()
+      cy.get('.single_add_to_cart_button').click()
+    });
+  });
